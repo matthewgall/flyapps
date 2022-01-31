977eb567445d8794fe3b7c335a0af4249fafbbcc
Matthew Gall <me@matthewgall.dev>
2022-01-31 15:18:27 +0000

    Moved from configuration file, to environment variables for config

--- ntfy/app/entrypoint.sh
+++ ntfy/app/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Load some variables
-export BASE_URL="${BASE_URL:-https://nfty-web.fly.dev}"
-
-# Fill in environment variables
-cat /etc/ntfy/server.yml.tpl | envsubst > /etc/ntfy/server.yml
-
 # Start the server
 echo "Starting ntfy"
 /usr/bin/ntfy serve
